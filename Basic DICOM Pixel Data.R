b65a763522cb67b03c95d2c488a81f6049ee38bc
Joon-Hwan-Hong <joon.hong@mail.mcgill.ca>
2020-02-06 23:04:38 -0500

    update to dicom and binary masking

--- Basic DICOM Pixel Data.R
+++ Basic DICOM Pixel Data.R
@@ -14,3 +14,20 @@ hist(slice$img[[1]][,], breaks=50, xlab="FLAIR", prob=T, col=rgb(0,0,1,1/4), mai
 # example of loading multiple DICOM files
 setwd("C:\\Users\\jhong43\\Documents\\Neuroimaging-with-R\\BRAINIX\\DICOM")
 all_slices_T1 = readDICOM("T1\\")
+
+
+
+# DICOM to NifTI
+# hard coded directory and extract dicoms
+setwd("C:\\Users\\jhong43\\Documents\\Neuroimaging-with-R\\BRAINIX\\DICOM")
+all_slices_T1 = readDICOM("T1\\")
+
+# to NifTI file
+nii_T1 = dicom2nifti(all_slices_T1)
+
+# write and read NifTI files
+setwd("C:\\Users\\jhong43\\Documents\\Neuroimaging-with-R\\BRAINIX\\NIfTI")
+f_name = "TEST_3D_File"
+writeNIfTI(nim=nii_T1, filename=f_name)
+
+nii_T2 = readNIfTI("T2.nii.gz", reorient= FALSE)
---
+++ Binary Masking and Comparison.R
@@ -0,0 +1,20 @@
+library(oro.nifti)
+
+# setting MRI directory for Kirby21 data
+mridir <- paste(getwd(), "/kirby21/visit_1/113", sep="")
+
+# TEST for reading image
+file.path(mridir, "SUBJ001-01-MPRAGE.nii.gz")
+T1 <- readNIfTI(file.path(paste0(mridir,"/SUBJ0001-01-MPRAGE.nii.gz")))
+orthographic(T1)
+
+# Binary Masking
+mask <- readNIfTI(file.path(paste0(mridir,"/SUBJ0001_mask.nii.gz")))
+orthographic(mask)
+masked.T1 <- T1*mask
+orthographic(masked.T1)
+
+# comparing different samples
+T1.followup <- readNIfTI(file.path(paste0(mridir,"/SUBJ0001-02-MPRAGE.nii.gz")))
+subtract.T1 <- T1.followup - T1
+orthographic(subtract.T1)
